b8e6ea84773e6459e0c99237cb3e2592049af4b7
Jesus <jesuslcorominas@gmail.com>
2026-03-25 07:09:14 +0100

    chore: bump version to 0.3.0 (build 17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.jesuslcorominas.teamflowmanager"
         minSdk = 29
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
