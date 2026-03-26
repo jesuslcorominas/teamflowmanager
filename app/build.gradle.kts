49746746144696c21df6a13c599d2d5a523974d3
Jesus <jesuslcorominas@gmail.com>
2026-03-26 08:33:07 +0100

    chore: bump version to 0.4.1 (build 43) [skip-tests]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.jesuslcorominas.teamflowmanager"
         minSdk = 29
         targetSdk = 36
-        versionCode = 42
+        versionCode = 43
         versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
