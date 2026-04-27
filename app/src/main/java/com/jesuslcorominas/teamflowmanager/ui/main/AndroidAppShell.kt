979c5f22ddbb3bde4504906034ff15ec3372dfb5
Jesus <jesuslcorominas@gmail.com>
2026-04-27 14:17:43 +0200

    style: ktlint auto-format

--- app/src/main/java/com/jesuslcorominas/teamflowmanager/ui/main/AndroidAppShell.kt
+++ app/src/main/java/com/jesuslcorominas/teamflowmanager/ui/main/AndroidAppShell.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
