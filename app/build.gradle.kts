deef9ced28ff67dc218d464768f5f51cdda3f68a
Jesus <jesuslcorominas@gmail.com>
2026-09-13 11:15:15 +0200

    chore: bump version to 0.7.0 (build 55)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.jesuslcorominas.teamflowmanager"
         minSdk = 29
         targetSdk = 36
-        versionCode = 54
-        versionName = "0.6.3"
+        versionCode = 55
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
